408383ad4adce1cfa2bf7d21672fd4a199dde2d1
Berke Fırat YILDIRIM <69518455+berkefiratto@users.noreply.github.com>
2025-06-13 22:42:41 +0300

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import LoginForm from "./components/LoginForm";
+import RegisterForm from "./components/RegisterForm";
+
+function App() {
+  return (
+    <div>
+      <LoginForm />
+      <hr />
+      <RegisterForm />
+    </div>
+  );
+}
+
+export default App;
