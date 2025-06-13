33182cb76acb412d0f1ac50cbcc035bde61ae954
Berke Fırat YILDIRIM <69518455+berkefiratto@users.noreply.github.com>
2025-06-13 22:42:31 +0300

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,13 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "XXXXXX",
-  appId: "XXXXXX"
+  apiKey: "-",
+  authDomain: "-",
+  projectId: "-",
+  storageBucket: "-",
+  messagingSenderId: "-",
+  appId: "-"
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+
+//Areas who contain '-' will be filled by the script you have taken from firebase while configuring a database.
